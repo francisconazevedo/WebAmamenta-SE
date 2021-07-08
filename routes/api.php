88d1b16955359a7be5b806d0457588a8013c7490
Francisco Azevedo <flneto.azevedo@gmail.com>
2021-07-08 14:26:09 -0300

    Endpoints to list informations to app

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ConfigurationController;
 use App\Http\Controllers\DonaterController;
+use App\Http\Controllers\LinkController;
+use App\Http\Controllers\MythOrTruthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,4 +25,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::prefix('v1')->group(function (){
    Route::apiResource('donaters', DonaterController::class)->only('store');
+   Route::apiResource('milkbanks', DonaterController::class)->only('index');
+   Route::apiResource('myths-or-truths', MythOrTruthController::class)->only('index');
+   Route::apiResource('links', LinkController::class)->only('index');
+   Route::get('how-donate', 'ConfigurationController@howDonate');
 });
